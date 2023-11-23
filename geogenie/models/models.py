3a54e43ae761d8e4014472bfff23e5425592b9a3
btmartin721 <evobio721@gmail.com>
2023-11-23 02:09:58 -0600

    added transformer (functional) and GNN models

--- geogenie/models/models.py
+++ geogenie/models/models.py
@@ -1,4 +1,9 @@
 import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from torch_geometric.nn import GCNConv
+from torch import mean as torchmean
+from geogenie.utils.utils import base_to_int
 
 
 class MLPRegressor(nn.Module):
@@ -56,3 +61,79 @@ class MLPRegressor(nn.Module):
         if self.extra_layer is not None:
             x = self.extra_layer(x)
         return self.output_layer(x)
+
+
+class SNPTransformer(nn.Module):
+    def __init__(
+        self,
+        input_size,
+        width=256,
+        nlayers=3,
+        dropout_prop=0.1,
+        device="cpu",
+        embedding_dim=256,
+        nhead=8,
+        dim_feedforward=1024,
+        **kwargs,
+    ):
+        super(SNPTransformer, self).__init__()
+        self.device = device
+        self.d_model = embedding_dim
+        num_bases = len(base_to_int())  # Number of unique bases
+        self.embedding = nn.Embedding(
+            num_embeddings=num_bases, embedding_dim=embedding_dim, device=device
+        )
+        encoder_layer = nn.TransformerEncoderLayer(
+            d_model=embedding_dim,
+            nhead=nhead,
+            dim_feedforward=dim_feedforward,
+            dropout=dropout_prop,
+            device=self.device,
+            batch_first=True,
+        )
+        self.transformer_encoder = nn.TransformerEncoder(
+            encoder_layer, num_layers=nlayers
+        )
+        self.fc_out = nn.Linear(embedding_dim, 2, device=self.device)
+
+    def forward(self, src):
+        src = self.embedding(src) * np.sqrt(self.d_model)
+        src = src.permute(1, 0, 2)  # expects seq_len, batch, input_size
+        output = self.transformer_encoder(src)
+        output = output.permute(1, 0, 2)
+        output = F.relu(output.mean(dim=1))  # Aggregating over the sequence
+        return self.fc_out(output)
+
+
+class GeoRegressionGNN(nn.Module):
+    def __init__(
+        self,
+        input_size,
+        width=16,
+        nlayers=3,
+        dropout_prop=0.1,
+        device="cpu",
+        **kwargs,
+    ):
+        super(GeoRegressionGNN, self).__init__()
+        self.width = width
+        self.nlayers = nlayers
+        self.dropout_prop = dropout_prop
+        self.device = device
+        self.layers = nn.ModuleList()
+        self.layers.append(GCNConv(input_size, width, device=self.device))
+
+        for _ in range(nlayers - 1):
+            self.layers.append(GCNConv(width, width, device=self.device))
+
+        self.fc = nn.Linear(width, 2, device=self.device)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+
+        for layer in self.layers:
+            x = F.elu(layer(x, edge_index))
+            x = F.dropout(x, p=self.dropout_prop, training=self.training)
+
+        x = torchmean(x, dim=0)
+        return self.fc(x)
