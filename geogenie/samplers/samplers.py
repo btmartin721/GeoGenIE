925942752c94b774defc6abe2d4db8c6be8dab68
btmartin721 <evobio721@gmail.com>
2023-11-27 09:51:25 -0600

    added kernel density sampler

--- geogenie/samplers/samplers.py
+++ geogenie/samplers/samplers.py
@@ -1,7 +1,9 @@
 import logging
 
 import numpy as np
+from sklearn.neighbors import KernelDensity
 from torch import from_numpy
+from torch.utils.data import Sampler
 
 
 class Samplers:
@@ -32,3 +34,53 @@ class Samplers:
 
         samples_weight = from_numpy(samples_weight)  # pytorch.
         return samples_weight
+
+
+class GeographicDensitySampler(Sampler):
+    """
+    A PyTorch sampler that samples based on geographic density.
+
+    Attributes:
+        indices (list of int): List of indices to sample from.
+        weights (numpy array): Array of sample weights.
+    """
+
+    def __init__(self, data, bandwidth=0.1):
+        """
+        Args:
+            data (pandas DataFrame): DataFrame containing 'x' (longitude) and 'y' (latitude).
+            bandwidth (float): Bandwidth for KDE. Adjust for more/less smoothing.
+        """
+        self.data = data
+        self.bandwidth = bandwidth
+        self.indices = np.arange(len(data))
+        self.weights = self.calculate_weights()
+
+    def calculate_weights(self):
+        """
+        Calculate weights based on geographic density using Kernel Density Estimation.
+
+        Returns:
+            numpy array: Weights for each sample.
+        """
+        coords = self.data.copy()
+        kde = KernelDensity(bandwidth=self.bandwidth, kernel="gaussian")
+        kde.fit(coords)
+        log_density = kde.score_samples(coords)
+        density = np.exp(log_density)
+        # Adding a small constant to avoid division by zero
+        weights = 1 / (density + 1e-5)
+        return weights
+
+    def __iter__(self):
+        return (
+            self.indices[i]
+            for i in np.random.choice(
+                self.indices,
+                size=len(self.indices),
+                p=self.weights / np.sum(self.weights),
+            )
+        )
+
+    def __len__(self):
+        return len(self.data)
