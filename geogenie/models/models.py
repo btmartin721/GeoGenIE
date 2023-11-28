ce4fbab5a5928c3e47e27cc94753ed698c54bf8d
btmartin721 <evobio721@gmail.com>
2023-11-28 17:20:54 -0600

    added tapered widths

--- geogenie/models/models.py
+++ geogenie/models/models.py
@@ -1,3 +1,4 @@
+import logging
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
@@ -17,35 +18,52 @@ class MLPRegressor(nn.Module):
         nlayers=10,
         dropout_prop=0.2,
         device="cpu",
+        output_width=2,
+        min_width=3,
+        factor=0.5,
         **kwargs,
     ):
         super(MLPRegressor, self).__init__()
         self.device = device
+
+        self.logger = logging.getLogger(__name__)
+
+        initial_width = width
+        if width >= input_size:
+            self.logger.warning(
+                "Provided hidden layer width is >= number of input features. Reducing initial layer width."
+            )
+
+        while width >= input_size:
+            width *= 0.8
+            width = int(width)
+
+        if initial_width >= input_size:
+            self.logger.warning(f"Reduced initial hidden layer width: {width}")
+
         self.input_layer = nn.Linear(input_size, width, device=self.device)
         self.elu = nn.ELU()
         self.dropout = nn.Dropout(dropout_prop)
 
-        self.layers = nn.ModuleList()
+        if nlayers < 2:
+            raise ValueError(
+                "Number of layers must be at least 2 to calculate a scaling factor."
+            )
 
-        # Creating blocks of layers with residual connections
         # self.blocks = nn.ModuleList()
         # for _ in range(nlayers // 2):  # Handles even numbers of layers
-        inwidth = width
-        for _ in range(nlayers):
-            outwidth = int(inwidth * 0.9)
-            if outwidth > 2:
-                self.layers.append(
-                    nn.Sequential(
-                        nn.Linear(inwidth, outwidth, device=self.device),
-                        nn.BatchNorm1d(outwidth, outwidth, device=self.device),
-                        nn.Dropout(dropout_prop),
-                    )
-                )
-                inwidth = outwidth
-            else:
-                break
+        #     self.blocks.append(
+        #         nn.Sequential(
+        #             nn.Linear(width, width, device=self.device),
+        #             nn.BatchNorm1d(width, device=self.device),
+        #             nn.ELU(),
+        #             nn.Dropout(dropout_prop),
+        #             nn.Linear(width, width, device=self.device),
+        #             nn.BatchNorm1d(width, device=self.device),
+        #         )
+        #     )
 
-        # Adding an additional layer if nlayers is odd
+        # # Adding an additional layer if nlayers is odd
         # self.extra_layer = None
         # if nlayers % 2 != 0:
         #     self.extra_layer = nn.Sequential(
@@ -54,22 +72,104 @@ class MLPRegressor(nn.Module):
         #         nn.ELU(),
         #         nn.Dropout(dropout_prop),
         #     )
+        # self.output_layer = nn.Linear(width, output_width, device=device)
 
-        self.output_layer = nn.Linear(inwidth, 2, device=self.device)
+        new_width = width
+        old_width = width
+        self.blocks = nn.ModuleList()
+        for _ in range(nlayers // 2):  # Handles even numbers of layers
+            new_width *= factor
+            new_width = int(new_width)
+            if new_width >= min_width:
+                self.blocks.append(
+                    nn.Sequential(
+                        nn.Linear(old_width, new_width, device=self.device),
+                        nn.BatchNorm1d(new_width, device=self.device),
+                        nn.ELU(),
+                        nn.Dropout(dropout_prop),
+                        nn.Linear(new_width, new_width, device=self.device),
+                        nn.BatchNorm1d(new_width, device=self.device),
+                    )
+                )
+                old_width = new_width
+                final_width = new_width
+            else:
+                final_width = old_width
+                break
+
+        # Adding an additional layer if nlayers is odd
+        self.extra_layer = None
+        if nlayers % 2 != 0:
+            self.extra_layer = nn.Sequential(
+                nn.Linear(final_width, final_width, device=self.device),
+                nn.BatchNorm1d(final_width, device=self.device),
+                nn.ELU(),
+                nn.Dropout(dropout_prop),
+            )
+        self.output_layer = nn.Linear(final_width, output_width, device=device)
+
+        # new_width = width
+        # old_width = width
+        # # Creating blocks of layers with residual connections
+        # self.blocks = nn.ModuleList()
+        # for _ in range(nlayers // 2):  # Handles even numbers of layers
+        #     new_width *= factor
+        #     new_width = int(new_width)
+        #     if new_width >= min_width:
+        #         self.blocks.append(
+        #             nn.Sequential(
+        #                 nn.Linear(old_width, new_width, device=self.device),
+        #                 nn.BatchNorm1d(new_width, device=self.device),
+        #                 nn.ELU(),
+        #                 nn.Dropout(dropout_prop),
+        #                 nn.Linear(new_width, new_width, device=self.device),
+        #                 nn.BatchNorm1d(new_width, device=self.device),
+        #             )
+        #         )
+        #         old_width = new_width
+        #         final_width = new_width
+        #     else:
+        #         final_width = old_width
+        #         break
+
+        # # Adding an additional layer if nlayers is odd
+        # self.extra_layer = None
+        # if nlayers % 2 != 0:
+        #     self.extra_layer = nn.Sequential(
+        #         nn.Linear(final_width, final_width, device=self.device),
+        #         nn.BatchNorm1d(final_width, device=self.device),
+        #         nn.ELU(),
+        #         nn.Dropout(dropout_prop),
+        #     )
+        # self.output_layer = nn.Linear(final_width, output_width, device=device)
 
     def forward(self, x):
         """Forward pass through network."""
         x = self.elu(self.input_layer(x))
-        for layer in self.layers:
-            x = self.elu(layer(x))
-        # Applying residual blocks
-        # for block in self.blocks:
-        # residual = x
-        # Add the residual (skip connection)
-        # x = self.elu(block(x) + residual)
-        # if self.extra_layer is not None:
-        # x = self.extra_layer(x)
+        for block in self.blocks:
+            residual = x  # Storing residual
+            x = block(x)  # Applying block
+
+            # Adjust residual if necessary
+            residual = self.adjust_residual(residual, block)
+            x += residual
+            x = self.elu(x)
+
+        if self.extra_layer is not None:
+            x = self.extra_layer(x)
         return self.output_layer(x)
+
+    def adjust_residual(self, residual, block):
+        """Adjust the residual to match the output dimension of the block."""
+        # Assuming block[0] is the first Linear layer in the block
+        out_features = block[0].out_features
+        if residual.shape[-1] != out_features:
+            # Dynamically adjust the residual adjuster to match the output dimensions
+            self.residual_adjuster = nn.Linear(
+                residual.shape[-1], out_features, device=self.device
+            )
+            residual = self.residual_adjuster(residual)
+        return residual
 
 
 class SNPTransformer(nn.Module):
