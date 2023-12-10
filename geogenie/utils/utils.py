9565ad4e9b66f69eb26832be1b37aaffa5c5a126
btmartin721 <evobio721@gmail.com>
2023-12-10 10:20:47 -0600

    CustomDataset, rmse_to_distance

--- geogenie/utils/utils.py
+++ geogenie/utils/utils.py
@@ -3,8 +3,152 @@ import sys
 from contextlib import contextmanager
 
 import numpy as np
+import scipy.optimize
+import torch
+from sklearn.base import BaseEstimator, TransformerMixin
+from torch.utils.data import Dataset
 
 logger = logging.getLogger(__name__)
+
+
+class CustomDataset(Dataset):
+    def __init__(self, features, labels, sample_weights=None):
+        """
+        Initialize custom PyTorch Dataset that incorporates sample weighting.
+
+        Args:
+            features (Tensor): Input features.
+            labels (Tensor): Labels corresponding to the features.
+            sample_weights (Tensor): Weights for each sample.
+        """
+        if not isinstance(features, torch.Tensor):
+            features = torch.tensor(features, dtype=torch.float32)
+        if not isinstance(labels, torch.Tensor):
+            labels = torch.tensor(labels, dtype=torch.float32)
+
+        if sample_weights is not None:
+            if not isinstance(sample_weights, torch.Tensor):
+                sample_weights = torch.tensor(sample_weights, dtype=torch.float32)
+
+        self.features = features
+        self.labels = labels
+        self.sample_weights = sample_weights
+
+        # Store the tensors as a tuple
+        self.tensors = (features, labels, sample_weights)
+
+    def __len__(self):
+        """
+        Return the total number of samples in the dataset.
+        """
+        return len(self.features)
+
+    def __getitem__(self, idx):
+        """
+        Retrieve the sample at the given index.
+
+        Args:
+            idx (int): Index of the sample to retrieve.
+
+        Returns:
+            tuple: (feature, label, sample_weight) for the specified index.
+        """
+
+        if self.sample_weights is None:
+            return self.features[idx], self.labels[idx]
+        else:
+            return (
+                self.features[idx],
+                self.labels[idx],
+                self.sample_weights[idx],
+            )
+
+
+class LongLatToCartesianTransformer(BaseEstimator, TransformerMixin):
+    """
+    A transformer for converting longitude and latitude to/ from Cartesian coordinates.
+
+    Attributes:
+        radius (float): The radius of the Earth in kilometers.
+    """
+
+    def __init__(self, radius=6371):
+        """
+        Initialize the transformer with the Earth's radius.
+
+        Args:
+            radius (float, optional): The radius of the Earth in kilometers. Defaults to 6371.
+        """
+        self.radius = radius
+
+    def fit(self, X, y=None):
+        """Fit method for compatibility with scikit-learn. Does nothing."""
+        return self
+
+    def transform(self, X):
+        """
+        Convert latitude and longitude to Cartesian coordinates.
+
+        Args:
+            X (array-like): The longitude and latitude values. Shape (n_samples, 2).
+
+        Returns:
+            np.ndarray: Cartesian coordinates. Shape (n_samples, 3).
+        """
+        lon_rad = np.radians(X[:, 0])
+        lat_rad = np.radians(X[:, 1])
+
+        x = self.radius * np.cos(lat_rad) * np.cos(lon_rad)
+        y = self.radius * np.cos(lat_rad) * np.sin(lon_rad)
+        z = self.radius * np.sin(lat_rad)
+
+        return np.column_stack((x, y, z))
+
+    def inverse_transform(self, X):
+        """
+        Convert Cartesian coordinates back to latitude and longitude.
+
+        Args:
+            X (array-like): Cartesian coordinates. Shape (n_samples, 3).
+
+        Returns:
+            np.ndarray: Longitude and latitude values. Shape (n_samples, 2).
+        """
+        x, y, z = X[:, 0], X[:, 1], X[:, 2]
+        lat = np.degrees(np.arcsin(z / self.radius))
+        lon = np.degrees(np.arctan2(y, x))
+
+        return np.column_stack((lon, lat))
+
+
+def custom_gpr_optimizer(obj_func, initial_theta, bounds):
+    """
+    Custom optimizer using scipy.optimize.minimize with increased maxiter.
+
+    Args:
+        obj_func (callable): The objective function.
+        initial_theta (array-like): Initial guess for the parameters.
+        bounds (list of tuples): Bounds for the parameters.
+
+    Returns:
+        tuple: Optimized parameters and function value at the optimum.
+    """
+    # Call scipy.optimize.minimize with the necessary parameters
+    opt_res = scipy.optimize.minimize(
+        obj_func,
+        initial_theta,
+        method="L-BFGS-B",
+        jac=True,  # True if obj_func returns the gradient as well
+        bounds=bounds,
+        options={"maxiter": 15000, "eps": 1e-3},
+    )
+
+    # Check the result and return the optimized parameters
+    if not opt_res.success:
+        logger.warning(RuntimeWarning("Optimization failed: " + opt_res.message))
+
+    theta_opt, func_min = opt_res.x, opt_res.fun
+    return theta_opt, func_min
 
 
 def geo_coords_is_valid(coordinates):
@@ -37,6 +181,41 @@ def geo_coords_is_valid(coordinates):
         logger.error(msg)
         raise ValueError(msg)
     return True
+
+
+def rmse_to_distance(rmse, latitudes):
+    """
+    Convert RMSE in decimal degrees to distance in kilometers for a batch of predictions,
+    and calculate the mean, median, and standard deviation of the distance errors.
+
+    Args:
+        rmse (numpy.ndarray): RMSE values in decimal degrees, shape [batch_size, 2] ([rmse_longitude, rmse_latitude]).
+        latitudes (numpy.ndarray): Latitudes at which to estimate the distances, shape (n_samples,).
+
+    Returns:
+        dict: A dictionary containing the mean, median, and standard deviation of the error distances.
+    """
+    km_per_degree_latitude = 111  # Approximate value; better away from poles.
+    km_per_degree_longitude = km_per_degree_latitude * np.cos(np.radians(latitudes))
+
+    # Convert RMSE from degrees to kilometers
+    distance_longitude = rmse[:, 0] * km_per_degree_longitude
+    distance_latitude = rmse[:, 1] * km_per_degree_latitude
+
+    # Combine the latitudinal and longitudinal errors
+    total_distance_error = np.sqrt(distance_longitude**2 + distance_latitude**2)
+
+    # Calculate mean, median, and standard deviation
+    mean_error = np.mean(total_distance_error)
+    median_error = np.median(total_distance_error)
+    std_dev_error = np.std(total_distance_error)
+
+    return {
+        "mean": mean_error,
+        "median": median_error,
+        "std_dev": std_dev_error,
+        "dist_error": total_distance_error,
+    }
 
 
 class StreamToLogger:
