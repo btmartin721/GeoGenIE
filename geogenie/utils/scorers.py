64d6e9148c36ccf96fa8a434eb9f1152e80af2d8
btmartin721 <evobio721@gmail.com>
2023-12-15 01:47:12 -0600

    kstest, skewness

--- geogenie/utils/scorers.py
+++ geogenie/utils/scorers.py
@@ -4,9 +4,115 @@ from math import asin, cos, radians, sin, sqrt
 
 import numba
 import numpy as np
-from sklearn.metrics import r2_score
+import pandas as pd
+import scipy.stats as stats
+from sklearn.manifold import LocallyLinearEmbedding
+from sklearn.metrics import median_absolute_error, r2_score
 
 logger = logging.getLogger(__name__)
+
+
+def kstest(y_true, y_pred, sample_weight=None):
+    # Calculate Haversine error for each pair of points
+    haversine_errors = np.array(
+        [
+            haversine(act[0], act[1], pred[0], pred[1])
+            for act, pred in zip(y_true, y_pred)
+        ]
+    )
+
+    errors = np.array(haversine_errors)
+
+    # Statistical Distribution Analysis
+    mean_error = np.mean(errors)
+    std_dev_error = np.std(errors)
+    skewness_error = stats.skew(errors)
+
+    # Kolmogorov-Smirnov Test
+    ks_statistic, p_value = stats.kstest(
+        errors, "norm", args=(mean_error, std_dev_error)
+    )
+
+    return (
+        ks_statistic,
+        p_value,
+        skewness_error,
+    )
+
+
+class LocallyLinearEmbeddingWrapper(LocallyLinearEmbedding):
+    def predict(self, X):
+        return self.transform(X)
+
+    @staticmethod
+    def lle_reconstruction_scorer(estimator, X, y=None):
+        """
+        Compute the negative reconstruction error for an LLE model to use as a scorer.
+        GridSearchCV assumes that higher score values are better, so the reconstruction
+        error is negated.
+
+        Args:
+            estimator (LocallyLinearEmbedding): Fitted LLE model.
+            X (numpy.ndarray): Original high-dimensional data.
+
+        Returns:
+            float: Negative reconstruction error.
+        """
+        return -estimator.reconstruction_error_
+
+
+# def evaluate_outliers(
+#     train_samples, outlier_geo_indices, outlier_gen_indices, dt, args
+# ):
+#     if not isinstance(train_samples, pd.Series):
+#         train_samples = pd.Series(train_samples, name="ID")
+
+#     """Evaluate and plot results from GeoGenOutlierDetector."""
+#     y_pred = process_pred(train_samples, outlier_geo_indices, outlier_gen_indices, dt)
+#     y_true = process_truths(train_samples, dt)
+
+#     plotting = PlotGenIE(
+#         "cpu",
+#         args.output_dir,
+#         args.prefix,
+#         args.show_plots,
+#         args.fontsize,
+#     )
+#     plotting.plot_confusion_matrix(y_true["Label"], y_pred["Label"], dt)
+
+#     logger.info(f"Accuracy: {accuracy_score(y_true['Label'], y_pred['Label'])}")
+#     logger.info(f"F1 Score: {f1_score(y_true['Label'], y_pred['Label'])}")
+
+
+def process_truths(train_samples):
+    """Process true values."""
+    truths = pd.read_csv("data/real_outliers.csv", sep=" ")
+    truths["method"] = truths["method"].str.replace('"', "")
+    truths["method"] = truths["method"].str.replace("'", "")
+    y_true = pd.DataFrame(columns=["ID"])
+    y_true["ID"] = train_samples
+    y_true["Label"] = y_true["ID"].isin(truths["ID"]).astype(int)
+    y_true["Type"] = "True"
+    return y_true.sort_values(by=["ID"])
+
+
+def process_pred(train_samples, outlier_geo_indices, outlier_gen_indices, label_type):
+    """Process predictions."""
+    # Create the y_pred DataFrame
+    y_pred = pd.DataFrame(columns=["ID"])
+    y_pred["ID"] = train_samples
+    y_pred[label_type] = 0
+    y_pred["Type"] = "Pred"
+
+    # Convert outlier indices to IDs
+    outlier_geo_ids = train_samples.iloc[outlier_geo_indices]
+    outlier_gen_ids = train_samples.iloc[outlier_gen_indices]
+
+    outlier_ids = pd.concat([outlier_geo_ids, outlier_gen_ids])
+
+    # Mark the outliers in y_pred based on IDs
+    y_pred["Label"] = y_pred["ID"].isin(outlier_ids).astype(int)
+    return y_pred.sort_values(by=["ID"])
 
 
 def haversine_distances_agg(y_true, y_pred, func):
