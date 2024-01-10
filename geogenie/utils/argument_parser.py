e3734badaa1cf0ef2970fb301c1c645562703320
btmartin721 <evobio721@gmail.com>
2024-01-10 10:04:13 -0600

    cleaned up options, added dtype option

--- geogenie/utils/argument_parser.py
+++ geogenie/utils/argument_parser.py
@@ -213,7 +213,7 @@ def setup_parser():
     )
     embed_group.add_argument(
         "--embedding_type",
-        type=str,
+        type=validate_lower_str,
         default="pca",
         help="Embedding to use with input SNP dataset. Supported options are: 'pca', 'polynomial', 'tsne', 'none' (no embedding). Default: 'pca'.",
     )
@@ -276,15 +276,15 @@ def setup_parser():
         help="Specify filename to load best paramseters from previous Optuna parameter search. Default: None (don't load best parameters).",
     )
     model_group.add_argument(
-        "--use_random_forest",
-        action="store_true",
-        help="Whether to use RandomForest model instead of deep learning model.Default: False (use deep learning model).",
-    )
-
-    model_group.add_argument(
         "--use_gradient_boosting",
         action="store_true",
         help="Whether to use Gradient Boosting model instead of deep learning model. Default: False (use deep learning model).",
+    )
+    model_group.add_argument(
+        "--dtype",
+        type=validate_lower_str,
+        default="float32",
+        help="PyTorch data type to use. Supported options include: 'float32 and 'float64'. 'float64' is more accurate, but uses more memory and also is not supported with GPUs. Default: 'float32'.",
     )
 
     # Training Parameters
@@ -332,12 +332,6 @@ def setup_parser():
         type=validate_split,
         default=0.1,
         help="Validation data proportion (0-1). Default: 0.15.",
-    )
-    training_group.add_argument(
-        "--class_weights",
-        action="store_true",
-        default=False,
-        help="Apply class weights to account for imbalanced geographic sampling density'. Default: False",
     )
     training_group.add_argument(
         "--do_bootstrap",
@@ -391,7 +385,7 @@ def setup_parser():
     geo_sampler_group = parser.add_argument_group("Geographic Density Sampler")
     geo_sampler_group.add_argument(
         "--use_weighted",
-        type=str,
+        type=validate_lower_str,
         help="Use inverse-weighted probability sampling to calculate sample weights based on sampling density; use the sample weights in the loss function, or both. Valid options include: 'sampler', 'loss', 'both', or 'none'. Default: 'none'.",
     )
     geo_sampler_group.add_argument(
@@ -552,7 +546,7 @@ def setup_parser():
     )
     plotting_group.add_argument(
         "--filetype",
-        type=str,
+        type=validate_lower_str,
         default="png",
         help="File type to use for plotting. Valid options include any that 'matplotlib.pyplot.savefig' supports. Most common options include 'png' or 'pdf', but the following are supported: (eps, jpeg, jpg, pdf, pgf, png, ps, raw, rgba, svg, svgz, tif, tiff, webp). Do not prepend a '.' character to the string. Default: 'png'.",
     )
@@ -599,72 +593,9 @@ def setup_parser():
         default=0.1,
         help="Buffer to add to the sampling bounding box on map visualizations. Adjust to your liking. Default: 0.1.",
     )
-
-    rf_group = parser.add_argument_group(
-        title="Random Forest Argument Group",
-        description="Specify parameters for doing Random Forest decision tree predictions.",
-    )
-
-    # Random Forest Parameters
-    rf_group.add_argument(
-        "--n_estimators",
-        type=validate_positive_int,
-        default=100,
-        help="Number of estimators to use for Random Forest Regressor. Default: 100.",
-    )
-
-    rf_group.add_argument(
-        "--criterion",
-        type=str,
-        default="squared_error",
-        help="Criterion to use for Random Forest Regressor. Valid options: 'squared_error', 'absolute_error', 'friedman_mse', 'poisson'. Default: 'squared_error'.",
-    )
-
-    rf_group.add_argument(
-        "--max_depth",
-        default=None,
-        help="Max Depth of tree for Random Forest Regressor. Default: None (expand until exhausted).",
-    )
-
-    rf_group.add_argument(
-        "--min_samples_split",
-        default=2,
-        help="The minimum number of samples required to split an internal node. Can be float or int. If float, then gets proportion from n_samples. If int, then gets the exact number of samples. int must be <= the total number of samples. Default: 2",
-    )
-
-    rf_group.add_argument(
-        "--min_samples_leaf",
-        default=1,
-        help="The minimum number of samples required to be at a leaf node. Can be int or float. If int, uses that exact number. If float, then must be between 0 and 1 and calculates proportion of n_samples. Default: 1.",
-    )
-
-    rf_group.add_argument(
-        "--max_features",
-        default=1.0,
-        help="The number of features to consider when looking for the best split. Can be 'sqrt', 'log2', float, or None. If float, then gets proportion of n_features and must be in interval (0, 1]. If None, uses all features. Default: 1.0 (use all features).",
-    )
-
-    rf_group.add_argument(
-        "--bootstrap",
-        action="store_true",
-        help="Whether bootstrap samples are used when building trees. If False, the whole dataset is used to build each tree. Default: True (do boostrapping).",
-    )
-
-    rf_group.add_argument(
-        "--oob_score",
-        action="store_true",
-        help="Whether to use out-of-bag samples to estimate the generalization score. Only used if '--bootstrap' is True. Default: False.",
-    )
-
-    rf_group.add_argument(
-        "--max_samples",
-        default=None,
-        help="Can be int, float, or None. If None, use all samples. If float, then get proportion of samples and must be between 0 and 1. If int, then get 'max_samples' samples. Default: None (use all samples).",
-    )
-
     gb_group = parser.add_argument_group(
         title="Gradient Boosting Argument Group",
-        description="Specify parameters for doing Gradient Boosting predictions.",
+        description="Specify parameters for doing XGBoost predictions.",
     )
     gb_group.add_argument(
         "--gb_learning_rate",
@@ -763,47 +694,97 @@ def setup_parser():
     args = parser.parse_args()
 
     # Load and apply configuration file if provided
-    if args.config:
-        if not os.path.exists(args.config):
-            parser.error(f"Configuration file not found: {args.config}")
-        config = load_config(args.config)
+    validate_inputs(parser, args)
+    validate_significance_levels(parser, args)
+    validate_max_neighbors(parser, args)
+    validate_embeddings(parser, args)
+    validate_seed(args)
+    validate_weighted_opts(parser, args)
+    validate_colorscale(parser, args)
+    validate_smote(parser, args)
+    validate_gb_params(parser, args)
+    validate_dtype(parser, args)
 
-        # Update default values based on the configuration file
-        for arg in vars(args):
-            if arg in config:
-                setattr(args, arg, config[arg])
+    return args
 
-    if args.sample_data is None:
-        logger.error("--sample_data argument is required.")
-        parser.error("--sample_data argument is required.")
 
-    if args.vcf is None and args.gtseq is None:
-        logger.error("Either --vcf or --gtseq must be defined.")
-        parser.error("Either --vcf or --gtseq must be defined.")
-
-    if args.vcf is not None and args.gtseq is not None:
-        logger.error("Only one of --vcf and --gtseq can be provided.")
-        parser.error("Only one of --vcf and --gtseq can be provided.")
-
-    if args.significance_level >= 1.0 or args.significance_level <= 0:
-        msg = f"'significance_level' must be between 0 and 1: {args.significance_level}"
+def validate_dtype(parser, args):
+    if args.dtype not in ["float64", "float32"]:
+        msg = f"'--dtype' argument must be either 'float64' or 'float32', but got: {args.dtype}"
         logger.error(msg)
         parser.error(msg)
 
-    if args.significance_level >= 0.5:
+
+def validate_gb_params(parser, args):
+    if args.gb_objective not in [
+        "reg:squarederror",
+        "reg:squaredlogerror",
+        "reg:absoluteerror",
+    ]:
+        msg = f"Invalid 'gb_objective' parameter provided. Supported options include: 'reg:squarederror', 'reg:squaredlogerror', 'reg:absoluteerror', but got: {args.gb_objective}"
+        logger.error(msg)
+        parser.error(msg)
+
+    if args.gb_eval_metric not in [
+        "rmse",
+        "rmsle",
+        "mae",
+        "mape",
+    ]:
+        msg = f"Invalid parameter provided to 'gb_eval_metric'. Supported options include: 'rmse', 'rmsle', 'mae', 'mape', but got: {args.gb_eval_metric}."
+        logger.error(msg)
+        parser.error(msg)
+
+    if args.gb_multi_strategy not in ["one_output_per_tree", "multi_output_tree"]:
+        msg = f"Invalid parameter provided to 'gb_multi_strategy'. Supported options include 'one_output_per_tree' or 'multi_output_tree', but got: {args.gb_multi_strategy}."
+        logger.error(msg)
+        parser.error(msg)
+
+    if args.gb_subsample > 1.0 or args.gb_subsample <= 0.0:
+        msg = f"Invalid value provided for 'gb_subsample'. Values must be > 0.0 and <= 1.0, but got: {args.gb_subsample}"
+        logger.error(msg)
+        parser.error(msg)
+
+
+def validate_weighted_opts(parser, args):
+    if args.use_weighted not in ["sampler", "loss", "both", "none"]:
+        msg = f"Invalid option passed to 'use_weighted': {args.use_weighted}"
+        logger.error(msg)
+        parser.error(msg)
+
+
+def validate_colorscale(parser, args):
+    if args.min_colorscale < 0:
+        msg = f"'--min_colorscale' must be >= 0: {args.min_colorscale}"
+        logger.error(msg)
+        parser.error(msg)
+
+    if args.max_colorscale <= args.min_colorscale:
+        msg = f"'--max_colorscale must be > --min_colorscale', but got: {args.min_colorscale}, {args.max_colorscale}"
+        logger.error(msg)
+        parser.error(msg)
+
+
+def validate_smote(parser, args):
+    if args.oversample_method not in [
+        "kmeans",
+        "optics",
+        "kerneldensity",
+        "none",
+    ]:
+        msg = f"'--oversample_method' value must be one of 'kmeans', 'optics', 'kerneldensity', or 'none', but got: {args.oversample_method}'"
+        logger.error(msg)
+        parser.error(msg)
+
+
+def validate_seed(args):
+    if args.seed is not None and args.embedding_type == "polynomial":
         logger.warning(
-            f"'significance_level' was set to a high number: {args.significance_level}. Outliers are removed if the P-values are <= 'significance_level' (e.g., if P <= 0.05). Are you sure this is what you want?"
+            "'polynomial' embedding does not support a random seed, but a random seed was supplied to the 'seed' argument."
         )
 
-    if args.max_neighbors <= 1:
-        logger.error(f"max_neighbors must be > 1: {args.max_neighbors}.")
-        parser.error(f"max_neighbors must be > 1: {args.max_neighbors}.")
 
-    if args.maxk <= 1:
-        msg = f"max_neighbors must be > 1: {args.maxk}."
-        logger.error(msg)
-        parser.error(msg)
-
+def validate_embeddings(parser, args):
     if args.embedding_type.lower() not in [
         "pca",
         "tsne",
@@ -825,16 +806,6 @@ def setup_parser():
             warnings.warn(msg)
             logger.warning(msg)
 
-    if args.embedding_type.lower() == "none":
-        msg = "'--embedding_type' was set to 'none', which means no embedding will be performed. Did you intend to do this?"
-        logger.warning(msg)
-        warnings.warn(msg)
-
-    if args.seed is not None and args.embedding_type == "polynomial":
-        logger.warning(
-            "'polynomial' embedding does not support a random seed, but a random seed was supplied to the 'seed' argument."
-        )
-
     if args.n_components is not None:
         if args.n_components > 3 and args.embedding_type in ["tsne", "mds"]:
             msg = f"n_components must set to 2 or 3 to use 'tsne' and 'mds', but got: {args.n_components}"
@@ -846,130 +817,49 @@ def setup_parser():
         logger.error(msg)
         parser.error(msg)
 
-    if args.use_weighted not in ["sampler", "loss", "both", "none"]:
-        msg = f"Invalid option passed to 'use_weighted': {args.use_weighted}"
+
+def validate_max_neighbors(parser, args):
+    if args.max_neighbors <= 1:
+        logger.error(f"max_neighbors must be > 1: {args.max_neighbors}.")
+        parser.error(f"max_neighbors must be > 1: {args.max_neighbors}.")
+
+    if args.maxk <= 1:
+        msg = f"max_neighbors must be > 1: {args.maxk}."
         logger.error(msg)
         parser.error(msg)
 
-    if args.min_colorscale < 0:
-        msg = f"'--min_colorscale' must be >= 0: {args.min_colorscale}"
+
+def validate_significance_levels(parser, args):
+    if args.significance_level >= 1.0 or args.significance_level <= 0:
+        msg = f"'significance_level' must be between 0 and 1: {args.significance_level}"
         logger.error(msg)
         parser.error(msg)
 
-    if args.oversample_method not in [
-        "kmeans",
-        "optics",
-        "kerneldensity",
-        "none",
-    ]:
-        msg = f"'--oversample_method' value must be one of 'kmeans', 'optics', 'kerneldensity', or 'none', but got: {args.oversample_method}'"
-        logger.error(msg)
-        parser.error(msg)
+    if args.significance_level >= 0.5:
+        logger.warning(
+            f"'significance_level' was set to a high number: {args.significance_level}. Outliers are removed if the P-values are <= 'significance_level' (e.g., if P <= 0.05). Are you sure this is what you want?"
+        )
 
-    if args.use_random_forest and args.use_gradient_boosting:
-        msg = "Cannot toggle both '--use_random_forest' and '--use_gradient_boosting' at the same time. Use one or the other."
-        logger.error(msg)
-        parser.error(msg)
 
-    if isinstance(args.max_depth, float):
-        if args.max_depth <= 0 or args.max_depth > 1.0:
-            msg = f"'max_depth' must be between 0 and 1 if supplied as type float: {args.max_depth}"
-            logger.error(msg)
-            parser.error(msg)
-    elif isinstance(args.max_depth, int):
-        if args.max_depth < 1:
-            msg = (
-                f"'max_depth' must be > 1 if type integer is supplied: {args.max_depth}"
-            )
-            logger.error(msg)
-            parser.error(msg)
+def validate_inputs(parser, args):
+    if args.config:
+        if not os.path.exists(args.config):
+            parser.error(f"Configuration file not found: {args.config}")
+        config = load_config(args.config)
 
-    if args.max_features is not None:
-        try:
-            float(args.max_features)
-        except TypeError:
-            if args.max_features not in ["sqrt", "log2"]:
-                msg = f"'--max_features' must be either 'sqrt' or 'log2' if of type string: {args.max_features}"
-                logger.error(msg)
-                parser.error(msg)
+        # Update default values based on the configuration file
+        for arg in vars(args):
+            if arg in config:
+                setattr(args, arg, config[arg])
 
-    if args.max_depth is not None:
-        if isinstance(args.max_depth, float):
-            if args.max_depth <= 0 or args.max_depth > 1.0:
-                msg = f"'--max_depth' must be in interval (0, 1] if type is float: {args.max_depth}."
-                logger.error(msg)
-                parser.error(msg)
+    if args.sample_data is None:
+        logger.error("--sample_data argument is required.")
+        parser.error("--sample_data argument is required.")
 
-    if args.filetype.startswith("."):
-        msg = f"'filetype' argument cannot start with a '.' character: {args.filetype}"
-        logger.error(msg)
-        parser.error(msg)
+    if args.vcf is None and args.gtseq is None:
+        logger.error("Either --vcf or --gtseq must be defined.")
+        parser.error("Either --vcf or --gtseq must be defined.")
 
-    if isinstance(args.min_samples_leaf, float):
-        if args.min_samples_leaf <= 0 or args.min_samples_leaf > 1.0:
-            msg = f"'--min_samples_leaf' must be in interval (0, 1] if type is float: {args.min_samples_leaf}."
-            logger.error(msg)
-            parser.error(msg)
-    elif isinstance(args.min_samples_leaf, int):
-        if args.min_samples_leaf <= 0:
-            msg = f"'--min_samples_leaf' must be > 0 if type is integer: {args.min_samples_leaf}."
-            logger.error(msg)
-            parser.error(msg)
-    if isinstance(args.min_samples_split, float):
-        if args.min_samples_split <= 0 or args.min_samples_split > 1.0:
-            msg = f"'--min_samples_split' must be in interval (0, 1] if type is float: {args.min_samples_split}."
-            logger.error(msg)
-            parser.error(msg)
-    elif isinstance(args.min_samples_split, int):
-        if args.min_samples_split <= 0:
-            msg = f"'--min_samples_split' must be > 0 if type is integer: {args.min_samples_split}."
-            logger.error(msg)
-            parser.error(msg)
-    if args.max_samples is not None:
-        if isinstance(args.max_samples, float):
-            if args.max_samples <= 0 or args.max_samples > 1.0:
-                msg = f"'--max_samples' must be in interval (0, 1] if type is float: {args.max_samples}."
-                logger.error(msg)
-                parser.error(msg)
-        elif isinstance(args.max_samples, int):
-            if args.max_samples <= 0:
-                msg = f"'--max_samples' must be > 0 if type is integer: {args.max_samples}."
-                logger.error(msg)
-                parser.error(msg)
-
-    if args.gb_objective not in [
-        "reg:squarederror",
-        "reg:squaredlogerror",
-        "reg:absoluteerror",
-        "reg:quantileerror",
-        "reg:gamma",
-        "reg:tweedie",
-    ]:
-        msg = f"Invalid 'gb_objective' parameter provided. Supported options include: 'reg:squarederror', 'reg:squaredlogerror', 'reg:absoluteerror', 'reg:quantileerror', 'reg:gamma', or 'reg:tweedie', but got: {args.gb_objective}"
-        logger.error(msg)
-        parser.error(msg)
-
-    if args.gb_eval_metric not in [
-        "rmse",
-        "rmsle",
-        "mae",
-        "mape",
-        "mphe",
-        "gamma-nloglik",
-        "tweedie-nloglik",
-    ]:
-        msg = f"Invalid parameter provided to 'gb_eval_metric'. Supported options include: 'rmse', 'rmsle', 'mae', 'mape', 'mphe', 'gamma-nloglik', or 'tweedie-nloglik', but got: {args.gb_eval_metric}."
-        logger.error(msg)
-        parser.error(msg)
-
-    if args.gb_multi_strategy not in ["one_output_per_tree", "multi_output_tree"]:
-        msg = f"Invalid parameter provided to 'gb_multi_strategy'. Supported options include 'one_output_per_tree' or 'multi_output_tree', but got: {args.gb_multi_strategy}."
-        logger.error(msg)
-        parser.error(msg)
-
-    if args.gb_subsample > 1.0 or args.gb_subsample <= 0.0:
-        msg = f"Invalid value provided for 'gb_subsample'. Values must be > 0.0 and <= 1.0, but got: {args.gb_subsample}"
-        logger.error(msg)
-        parser.error(msg)
-
-    return args
+    if args.vcf is not None and args.gtseq is not None:
+        logger.error("Only one of --vcf and --gtseq can be provided.")
+        parser.error("Only one of --vcf and --gtseq can be provided.")
