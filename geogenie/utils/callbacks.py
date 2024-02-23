6e7b495d8a4abb799cc5ccf4e717e152086196d8
btmartin721 <evobio721@gmail.com>
2024-02-23 13:58:50 -0600

    more modular callback_init method for earlystop, LRScheduler

--- geogenie/utils/callbacks.py
+++ geogenie/utils/callbacks.py
@@ -3,12 +3,15 @@ import os
 
 import numpy as np
 from torch import save as torchsave
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 
 class EarlyStopping:
     """Early stopping pytorch callback."""
 
-    def __init__(self, output_dir, prefix, patience=100, verbose=0, delta=0):
+    def __init__(
+        self, output_dir, prefix, patience=100, verbose=0, delta=0, trial=None
+    ):
         self.patience = patience
         self.verbose = verbose
         self.counter = 0
@@ -18,6 +21,7 @@ class EarlyStopping:
         self.delta = delta
         self.output_dir = output_dir
         self.prefix = prefix
+        self.trial = 0 if trial is None else trial.number
 
         self.logger = logging.getLogger(__name__)
 
@@ -31,7 +35,7 @@ class EarlyStopping:
             self.counter += 1
             if self.verbose >= 2:
                 self.logger.info(
-                    f"EarlyStopping counter: {self.counter} out of {self.patience}"
+                    f"EarlyStopping counter: {self.counter}/{self.patience}"
                 )
             if self.counter >= self.patience:
                 self.early_stop = True
@@ -48,7 +52,28 @@ class EarlyStopping:
             )
 
         checkpoint_fn = os.path.join(
-            self.output_dir, "models", f"{self.prefix}_checkpoint.pt"
+            self.output_dir, "models", f"{self.prefix}_trial{self.trial}_checkpoint.pt"
         )
         torchsave(model.state_dict(), checkpoint_fn)
         self.val_loss_min = val_loss
+
+
+def callback_init(optimizer, args, trial=None):
+    early_stopping = EarlyStopping(
+        output_dir=args.output_dir,
+        prefix=args.prefix,
+        patience=args.early_stop_patience,
+        verbose=args.verbose >= 2,
+        delta=0,
+        trial=trial,
+    )
+
+    lr_scheduler = ReduceLROnPlateau(
+        optimizer,
+        mode="min",
+        factor=args.lr_scheduler_factor,
+        patience=args.lr_scheduler_patience,
+        verbose=args.verbose >= 2,
+    )
+
+    return early_stopping, lr_scheduler
