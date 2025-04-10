04d649a696b6bf6b2c9fd0a5ed9376b9efe1b503
btmartin721 <evobio721@gmail.com>
2025-04-10 17:57:01 -0400

    Update Sphinx configuration to correct sys.path and version number

--- geogenie/docs/source/conf.py
+++ geogenie/docs/source/conf.py
@@ -8,26 +8,26 @@
 
 import os
 import sys
-            
-sys.path.insert(0, os.path.abspath('../'))
 
-project = 'GeoGenIE'
-copyright = '2024, Bradley T. Martin and Tyler K. Chafin'
-author = 'Bradley T. Martin and Tyler K. Chafin'
-release = '1.0.1'
+sys.path.insert(0, os.path.abspath("../../../"))
+
+project = "GeoGenIE"
+copyright = "2024, Bradley T. Martin and Tyler K. Chafin"
+author = "Bradley T. Martin and Tyler K. Chafin"
+release = "1.0.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.githubpages',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.todo',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.intersphinx',
-    'sphinx_autodoc_typehints',
-    'sphinxcontrib.bibtex',
+    "sphinx.ext.githubpages",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.todo",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinx_autodoc_typehints",
+    "sphinxcontrib.bibtex",
 ]
 
 # -- Link intersphinx to the scikit-learn documentation ------------------------
